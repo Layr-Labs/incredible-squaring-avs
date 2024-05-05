1716910e6c36e6093fae1e29754a44a24aaf6b27
Robert Leonard <leaonrd.robert.h@gmail.com>
2024-05-04 23:09:37 -0400

    Comment out adapter until remappings file works

--- contracts/src/PriceFeedAdapter.sol
+++ contracts/src/PriceFeedAdapter.sol
@@ -1,34 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+// import "chainlink/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract PriceFeedAdapter {
-    AggregatorV3Interface internal dataFeed;
+// contract PriceFeedAdapter {
+//     AggregatorV3Interface internal dataFeed;
 
-    /**
-     * Network: Sepolia
-     * Aggregator: BTC/USD
-     * Address: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
-     */
-    constructor() {
-        dataFeed = AggregatorV3Interface(
-            0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
-        );
-    }
+//     /**
+//      * Network: Sepolia
+//      * Aggregator: BTC/USD
+//      * Address: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
+//      */
+//     constructor() {
+//         dataFeed = AggregatorV3Interface(
+//             0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
+//         );
+//     }
 
-    /**
-     * Returns the latest answer.
-     */
-    function getChainlinkDataFeedLatestAnswer() public view returns (int) {
-        // prettier-ignore
-        (
-            /* uint80 roundID */,
-            int answer,
-            /*uint startedAt*/,
-            /*uint timeStamp*/,
-            /*uint80 answeredInRound*/
-        ) = dataFeed.latestRoundData();
-        return answer;
-    }
-}
+//     /**
+//      * Returns the latest answer.
+//      */
+//     function getChainlinkDataFeedLatestAnswer() public view returns (int) {
+//         // prettier-ignore
+//         (
+//             /* uint80 roundID */,
+//             int answer,
+//             /*uint startedAt*/,
+//             /*uint timeStamp*/,
+//             /*uint80 answeredInRound*/
+//         ) = dataFeed.latestRoundData();
+//         return answer;
+//     }
+// }
--- contracts/test/PriceFeedAdapter.t.sol
+++ contracts/test/PriceFeedAdapter.t.sol
@@ -1,18 +1,18 @@
-pragma solidity 0.8.10;
+// pragma solidity 0.8.12;
 
-import "forge-std/Test.sol";
-import "forge-std/console2.sol";
+// import "forge-std/Test.sol";
+// import "forge-std/console2.sol";
 
-import { PriceFeedAdapter } from "../src/PriceFeedAdapter.sol";
+// import { PriceFeedAdapter } from "../src/PriceFeedAdapter.sol";
 
-contract PriceAdapterTest {
-    PriceFeedAdapter internal priceFeedAdapter;
+// contract PriceAdapterTest {
+//     PriceFeedAdapter internal priceFeedAdapter;
 
-    function setUp() public {
-        priceFeedAdapter = new PriceFeedAdapter();
-    }
+//     function setUp() public {
+//         priceFeedAdapter = new PriceFeedAdapter();
+//     }
 
-    function test_fetch_btc_usd_price() {
-        console.log(priceFeedAdapter.getChainlinkDataFeedLatestAnswer());
-    }
-}
+//     function test_fetch_btc_usd_price() {
+//         console.log(priceFeedAdapter.getChainlinkDataFeedLatestAnswer());
+//     }
+// }
