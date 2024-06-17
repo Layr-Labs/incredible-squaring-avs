9e08885de33c643d879a769e80584fa8f30e4364
amateur_dev <me@dipeshsukhani.dev>
2024-06-17 20:14:27 +0800

    Updating the base test

--- contracts/test/SourcingBestAuditorTaskManager.t.sol
+++ contracts/test/SourcingBestAuditorTaskManager.t.sol
@@ -1,16 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.12;
 
-import "../src/IncredibleSquaringServiceManager.sol" as incsqsm;
-import {IncredibleSquaringTaskManager} from "../src/IncredibleSquaringTaskManager.sol";
+import "../src/SourcingBestAuditorServiceManager.sol" as incsqsm;
+import {SourcingBestAuditorTaskManager} from "../src/SourcingBestAuditorTaskManager.sol";
 import {BLSMockAVSDeployer} from "@eigenlayer-middleware/test/utils/BLSMockAVSDeployer.sol";
 import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
-contract IncredibleSquaringTaskManagerTest is BLSMockAVSDeployer {
-    incsqsm.IncredibleSquaringServiceManager sm;
-    incsqsm.IncredibleSquaringServiceManager smImplementation;
-    IncredibleSquaringTaskManager tm;
-    IncredibleSquaringTaskManager tmImplementation;
+contract SourcingBestAuditorTaskManagerTest is BLSMockAVSDeployer {
+    incsqsm.SourcingBestAuditorServiceManager sm;
+    incsqsm.SourcingBestAuditorServiceManager smImplementation;
+    SourcingBestAuditorTaskManager tm;
+    SourcingBestAuditorTaskManager tmImplementation;
 
     uint32 public constant TASK_RESPONSE_WINDOW_BLOCK = 30;
     address aggregator =
@@ -21,13 +21,13 @@ contract IncredibleSquaringTaskManagerTest is BLSMockAVSDeployer {
     function setUp() public {
         _setUpBLSMockAVSDeployer();
 
-        tmImplementation = new IncredibleSquaringTaskManager(
+        tmImplementation = new SourcingBestAuditorTaskManager(
             incsqsm.IRegistryCoordinator(address(registryCoordinator)),
             TASK_RESPONSE_WINDOW_BLOCK
         );
 
         // Third, upgrade the proxy contracts to use the correct implementation contracts and initialize them.
-        tm = IncredibleSquaringTaskManager(
+        tm = SourcingBestAuditorTaskManager(
             address(
                 new TransparentUpgradeableProxy(
                     address(tmImplementation),
