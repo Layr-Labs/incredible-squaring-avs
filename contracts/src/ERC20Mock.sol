c193dca4d3daf52c9b8335071727a192852911bf
steven <steven.nevins.js@gmail.com>
2024-10-17 12:42:53 -0400

    fix: formatting

--- contracts/src/ERC20Mock.sol
+++ contracts/src/ERC20Mock.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20Mock is ERC20 {
     constructor() ERC20("", "") {}
-    
+
     function mint(address account, uint256 amount) public {
         _mint(account, amount);
     }
