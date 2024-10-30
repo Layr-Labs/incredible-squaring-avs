a949f533691ab684532997a99be6823761ce2cf5
kachapah <60323455+Sidu28@users.noreply.github.com>
2024-10-29 17:23:32 -0700

    init

--- tests/anvil/start-anvil-chain-with-el-and-avs-deployed.sh
+++ tests/anvil/start-anvil-chain-with-el-and-avs-deployed.sh
@@ -13,22 +13,30 @@ set -a
 source ./utils.sh
 set +a
 
-cleanup() {
-    echo "Executing cleanup function..."
-    set +e
-    docker rm -f anvil
-    exit_status=$?
-    if [ $exit_status -ne 0 ]; then
-        echo "Script exited due to set -e on line $1 with command '$2'. Exit status: $exit_status"
-    fi
-}
-trap 'cleanup $LINENO "$BASH_COMMAND"' EXIT
 
-# start an anvil instance in the background that has eigenlayer contracts deployed
-# we start anvil in the background so that we can run the below script
-# anvil --load-state avs-and-eigenlayer-deployed-anvil-state.json &
-# FIXME: bug in latest foundry version, so we use this pinned version instead of latest
-start_anvil_docker $parent_path/avs-and-eigenlayer-deployed-anvil-state.json ""
+run_deployment() {
+    forge script script/deploy/devnet/M2_Deploy_From_Scratch.s.sol --rpc-url http://localhost:8545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --broadcast --sig "run(string memory configFile)" -- M2_deploy_from_scratch.anvil.config.json
+    echo "deployment done"
+    mv script/output/devnet/M2_from_scratch_deployment_data.json ../../../../script/output/31337/eigenlayer_deployment_output.json
+    mv script/output/devnet/M2_from_scratch_deployment_data.json.bak script/output/devnet/M2_from_scratch_deployment_data.json
+    echo "deployment output moved"
+}
+
+start_anvil() {
+    anvil
+    sleep 2
+}
+
+
+
+cd ../../contracts/lib/eigenlayer-middleware/lib/eigenlayer-contracts
+pwd
+# deployment overwrites this file, so we save it as backup, because we want that output in our local files, and not in the eigenlayer-contracts submodule files
+mv script/output/devnet/M2_from_scratch_deployment_data.json script/output/devnet/M2_from_scratch_deployment_data.json.bak
+
+
+start_anvil &
+run_deployment &
 
 cd ../../contracts
 # we need to restart the anvil chain at the correct block, otherwise the indexRegistry has a quorumUpdate at the block number
@@ -38,5 +46,3 @@ cd ../../contracts
 cast rpc anvil_mine 100 --rpc-url $RPC_URL
 echo "advancing chain... current block-number:" $(cast block-number)
 
-# Bring Anvil back to the foreground
-docker attach anvil
