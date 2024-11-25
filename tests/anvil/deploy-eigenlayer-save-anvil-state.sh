9b927fdd53674b9837441eaa2f5d7d6272b6e39a
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-11-25 12:23:38 -0300

    chore: add TODO comment

--- tests/anvil/deploy-eigenlayer-save-anvil-state.sh
+++ tests/anvil/deploy-eigenlayer-save-anvil-state.sh
@@ -29,6 +29,7 @@ start_anvil_docker "" $parent_path/eigenlayer-deployed-anvil-state.json
 cd ../../contracts/lib/eigenlayer-middleware/lib/eigenlayer-contracts
 # deployment overwrites this file, so we save it as backup, because we want that output in our local files, and not in the eigenlayer-contracts submodule files
 mv script/output/devnet/M2_from_scratch_deployment_data.json script/output/devnet/M2_from_scratch_deployment_data.json.bak
+# TODO: replace config with one that deploys a MockETH strategy
 # M2_Deploy_From_Scratch.s.sol prepends "script/testing/" to the configFile passed as input (M2_deploy_from_scratch.anvil.config.json)
 forge script script/deploy/devnet/M2_Deploy_From_Scratch.s.sol --rpc-url http://localhost:8545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --broadcast --sig "run(string memory configFile)" -- M2_deploy_from_scratch.anvil.config.json
 mv script/output/devnet/M2_from_scratch_deployment_data.json ../../../../script/output/31337/eigenlayer_deployment_output.json
