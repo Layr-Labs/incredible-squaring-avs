ec87bfe8e131454e092c1dee493649edee0b6fa6
ehsueh <emma.yc.hsueh@gmail.com>
2025-02-08 13:13:12 -0500

    aggregator wip

--- aggregator/aggregator.go
+++ aggregator/aggregator.go
@@ -7,81 +7,42 @@ import (
 	"time"
 
 	"github.com/Layr-Labs/eigensdk-go/logging"
-	"github.com/ethereum/go-ethereum/accounts/abi"
-	"golang.org/x/crypto/sha3"
-
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/event"
 	"github.com/Layr-Labs/eigensdk-go/chainio/clients"
-	sdkclients "github.com/Layr-Labs/eigensdk-go/chainio/clients"
-	"github.com/Layr-Labs/eigensdk-go/services/avsregistry"
 	blsagg "github.com/Layr-Labs/eigensdk-go/services/bls_aggregation"
-	"github.com/Layr-Labs/eigensdk-go/services/operatorsinfo"
-	oprsinfoserv "github.com/Layr-Labs/eigensdk-go/services/operatorsinfo"
-	sdktypes "github.com/Layr-Labs/eigensdk-go/types"
-	"github.com/Layr-Labs/incredible-squaring-avs/aggregator/types"
-	"github.com/Layr-Labs/incredible-squaring-avs/core"
-	"github.com/Layr-Labs/incredible-squaring-avs/core/chainio"
-	"github.com/Layr-Labs/incredible-squaring-avs/core/config"
+	"github.com/Layr-Labs/trading-algo-avs/aggregator/types"
+	"github.com/Layr-Labs/trading-algo-avs/core"
+	"github.com/Layr-Labs/trading-algo-avs/core/chainio"
+	"github.com/Layr-Labs/trading-algo-avs/core/config"
 
-	cstaskmanager "github.com/Layr-Labs/incredible-squaring-avs/contracts/bindings/IncredibleSquaringTaskManager"
+	tradingTaskManager "github.com/kaohaohan/TradeAlgoAVS-frontend/blob/b7dd2c8fb10844d6c565ce7c06ddddea662a48f4/blockchain/contracts/TradingAlgoAVS"
 )
 
 const (
-	// number of blocks after which a task is considered expired
-	// this hardcoded here because it's also hardcoded in the contracts, but should
-	// ideally be fetched from the contracts
+	// Number of blocks after which a task is considered expired
 	taskChallengeWindowBlock = 100
 	blockTimeSeconds         = 12 * time.Second
-	avsName                  = "incredible-squaring"
+	avsName                  = "trading-algo-avs"
 )
 
-// Aggregator sends tasks (numbers to square) onchain, then listens for operator signed TaskResponses.
-// It aggregates responses signatures, and if any of the TaskResponses reaches the QuorumThresholdPercentage for each quorum
-// (currently we only use a single quorum of the ERC20Mock token), it sends the aggregated TaskResponse and signature onchain.
-//
-// The signature is checked in the BLSSignatureChecker.sol contract, which expects a
-//
-//	struct NonSignerStakesAndSignature {
-//		uint32[] nonSignerQuorumBitmapIndices;
-//		BN254.G1Point[] nonSignerPubkeys;
-//		BN254.G1Point[] quorumApks;
-//		BN254.G2Point apkG2;
-//		BN254.G1Point sigma;
-//		uint32[] quorumApkIndices;
-//		uint32[] totalStakeIndices;
-//		uint32[][] nonSignerStakeIndices; // nonSignerStakeIndices[quorumNumberIndex][nonSignerIndex]
-//	}
-//
-// A task can only be responded onchain by having enough operators sign on it such that their stake in each quorum reaches the QuorumThresholdPercentage.
-// In order to verify this onchain, the Registry contracts store the history of stakes and aggregate pubkeys (apks) for each operators and each quorum. These are
-// updated everytime an operator registers or deregisters with the BLSRegistryCoordinatorWithIndices.sol contract, or calls UpdateStakes() on the StakeRegistry.sol contract,
-// after having received new delegated shares or having delegated shares removed by stakers queuing withdrawals. Each of these pushes to their respective datatype array a new entry.
-//
-// This is true for quorumBitmaps (represent the quorums each operator is opted into), quorumApks (apks per quorum), totalStakes (total stake per quorum), and nonSignerStakes (stake per quorum per operator).
-// The 4 "indices" in NonSignerStakesAndSignature basically represent the index at which to fetch their respective data, given a blockNumber at which the task was created.
-// Note that different data types might have different indices, since for eg QuorumBitmaps are updated for operators registering/deregistering, but not for UpdateStakes.
-// Thankfully, we have deployed a helper contract BLSOperatorStateRetriever.sol whose function getCheckSignaturesIndices() can be used to fetch the indices given a block number.
-//
-// The 4 other fields nonSignerPubkeys, quorumApks, apkG2, and sigma, however, must be computed individually.
-// apkG2 and sigma are just the aggregated signature and pubkeys of the operators who signed the task response (aggregated over all quorums, so individual signatures might be duplicated).
-// quorumApks are the G1 aggregated pubkeys of the operators who signed the task response, but one per quorum, as opposed to apkG2 which is summed over all quorums.
-// nonSignerPubkeys are the G1 pubkeys of the operators who did not sign the task response, but were opted into the quorum at the blocknumber at which the task was created.
-// Upon sending a task onchain (or receiving a NewTaskCreated Event if the tasks were sent by an external task generator), the aggregator can get the list of all operators opted into each quorum at that
-// block number by calling the getOperatorState() function of the BLSOperatorStateRetriever.sol contract.
+// Aggregator listens to trading strategy events from the TradingAlgoAVS smart contract,
+// collects operator outputs for (strategy, user, market) tasks, aggregates statistics,
+// and sends the verified response on-chain if operator responses are consistent.
 type Aggregator struct {
-	logger           logging.Logger
-	serverIpPortAddr string
-	avsWriter        chainio.AvsWriterer
-	// aggregation related fields
+	logger                logging.Logger
+	serverIpPortAddr      string
+	avsWriter             chainio.AvsWriterer
 	blsAggregationService blsagg.BlsAggregationService
-	tasks                 map[types.TaskIndex]cstaskmanager.IIncredibleSquaringTaskManagerTask
-	tasksMu               sync.RWMutex
-	taskResponses         map[types.TaskIndex]map[sdktypes.TaskResponseDigest]cstaskmanager.IIncredibleSquaringTaskManagerTaskResponse
-	taskResponsesMu       sync.RWMutex
+	strategies            map[uint256]tradingTaskManager.Strategy
+	subscriptions         map[uint256]map[common.Address]bool
+	strategiesMu          sync.RWMutex
+	subscriptionsMu       sync.RWMutex
 }
 
-// NewAggregator creates a new Aggregator with the provided config.
+// NewAggregator creates a new Aggregator for TradingAlgoAVS.
 func NewAggregator(c *config.Config) (*Aggregator, error) {
-
 	avsReader, err := chainio.BuildAvsReaderFromConfig(c)
 	if err != nil {
 		c.Logger.Error("Cannot create avsReader", "err", err)
@@ -94,172 +55,135 @@ func NewAggregator(c *config.Config) (*Aggregator, error) {
 		return nil, err
 	}
 
-	chainioConfig := sdkclients.BuildAllConfig{
+	clients, err := clients.BuildAll(clients.BuildAllConfig{
 		EthHttpUrl:                 c.EthHttpRpcUrl,
 		EthWsUrl:                   c.EthWsRpcUrl,
-		RegistryCoordinatorAddr:    c.IncredibleSquaringRegistryCoordinatorAddr.String(),
+		RegistryCoordinatorAddr:    c.TradingAlgoRegistryCoordinatorAddr.String(),
 		OperatorStateRetrieverAddr: c.OperatorStateRetrieverAddr.String(),
 		AvsName:                    avsName,
-		PromMetricsIpPortAddress:   ":9090",
-	}
-	clients, err := clients.BuildAll(chainioConfig, c.EcdsaPrivateKey, c.Logger)
+	}, c.EcdsaPrivateKey, c.Logger)
 	if err != nil {
-		c.Logger.Errorf("Cannot create sdk clients", "err", err)
+		c.Logger.Errorf("Cannot create SDK clients", "err", err)
 		return nil, err
 	}
 
-	operatorPubkeysService := oprsinfoserv.NewOperatorsInfoServiceInMemory(context.Background(), clients.AvsRegistryChainSubscriber, clients.AvsRegistryChainReader, nil, operatorsinfo.Opts{}, c.Logger)
-
-	// This is the same hash function used by the operator to hash the task response before signing it.
-	hashFunction := func(taskResponse sdktypes.TaskResponse) (sdktypes.TaskResponseDigest, error) {
-		// The order here has to match the field ordering of cstaskmanager.IIncredibleSquaringTaskManagerTaskResponse
-		taskResponseType, err := abi.NewType("tuple", "", []abi.ArgumentMarshaling{
-			{
-				Name: "referenceTaskIndex",
-				Type: "uint32",
-			},
-			{
-				Name: "numberSquared",
-				Type: "uint256",
-			},
-		})
-		if err != nil {
-			c.Logger.Error("Error creating taskResponseType")
-			return sdktypes.TaskResponseDigest{}, err
-		}
-		arguments := abi.Arguments{
-			{
-				Type: taskResponseType,
-			},
-		}
-
-		encodeTaskResponseByte, err := arguments.Pack(taskResponse)
-		if err != nil {
-			c.Logger.Error("Error Packing taskResponse", err)
-			return sdktypes.TaskResponseDigest{}, err
-		}
-
-		var taskResponseDigest [32]byte
-		hasher := sha3.NewLegacyKeccak256()
-		hasher.Write(encodeTaskResponseByte)
-		copy(taskResponseDigest[:], hasher.Sum(nil)[:32])
-
-		return taskResponseDigest, nil
-	}
-
-	avsRegistryService := avsregistry.NewAvsRegistryServiceChainCaller(avsReader, operatorPubkeysService, c.Logger)
-	blsAggregationService := blsagg.NewBlsAggregatorService(avsRegistryService, hashFunction, c.Logger)
+	blsAggregationService := blsagg.NewBlsAggregatorService(nil, nil, c.Logger)
 
 	return &Aggregator{
 		logger:                c.Logger,
 		serverIpPortAddr:      c.AggregatorServerIpPortAddr,
 		avsWriter:             avsWriter,
 		blsAggregationService: blsAggregationService,
-		tasks:                 make(map[types.TaskIndex]cstaskmanager.IIncredibleSquaringTaskManagerTask),
-		taskResponses:         make(map[types.TaskIndex]map[sdktypes.TaskResponseDigest]cstaskmanager.IIncredibleSquaringTaskManagerTaskResponse),
+		strategies:            make(map[uint256]tradingTaskManager.Strategy),
+		subscriptions:         make(map[uint256]map[common.Address]bool),
 	}, nil
 }
 
+// Start the aggregator to listen for new strategy and subscription events
 func (agg *Aggregator) Start(ctx context.Context) error {
-	agg.logger.Infof("Starting aggregator.")
-	agg.logger.Infof("Starting aggregator rpc server.")
-	go agg.startServer(ctx)
+	agg.logger.Infof("Starting Trading Algo AVS Aggregator.")
 
-	// TODO(soubhik): refactor task generation/sending into a separate function that we can run as goroutine
-	ticker := time.NewTicker(10 * time.Second)
-	agg.logger.Infof("Aggregator set to send new task every 10 seconds...")
-	defer ticker.Stop()
-	taskNum := int64(0)
-	// ticker doesn't tick immediately, so we send the first task here
-	// see https://github.com/golang/go/issues/17601
-	_ = agg.sendNewTask(big.NewInt(taskNum))
-	taskNum++
-
-	for {
-		select {
-		case <-ctx.Done():
-			return nil
-		case blsAggServiceResp := <-agg.blsAggregationService.GetResponseChannel():
-			agg.logger.Info("Received response from blsAggregationService", "blsAggServiceResp", blsAggServiceResp)
-			agg.sendAggregatedResponseToContract(blsAggServiceResp)
-		case <-ticker.C:
-			err := agg.sendNewTask(big.NewInt(taskNum))
-			taskNum++
-			if err != nil {
-				// we log the errors inside sendNewTask() so here we just continue to the next task
-				continue
-			}
-		}
-	}
-}
-
-func (agg *Aggregator) sendAggregatedResponseToContract(blsAggServiceResp blsagg.BlsAggregationServiceResponse) {
-	// TODO: check if blsAggServiceResp contains an err
-	if blsAggServiceResp.Err != nil {
-		agg.logger.Error("BlsAggregationServiceResponse contains an error", "err", blsAggServiceResp.Err)
-		// panicing to help with debugging (fail fast), but we shouldn't panic if we run this in production
-		panic(blsAggServiceResp.Err)
-	}
-	nonSignerPubkeys := []cstaskmanager.BN254G1Point{}
-	for _, nonSignerPubkey := range blsAggServiceResp.NonSignersPubkeysG1 {
-		nonSignerPubkeys = append(nonSignerPubkeys, core.ConvertToBN254G1Point(nonSignerPubkey))
-	}
-	quorumApks := []cstaskmanager.BN254G1Point{}
-	for _, quorumApk := range blsAggServiceResp.QuorumApksG1 {
-		quorumApks = append(quorumApks, core.ConvertToBN254G1Point(quorumApk))
-	}
-	nonSignerStakesAndSignature := cstaskmanager.IBLSSignatureCheckerNonSignerStakesAndSignature{
-		NonSignerPubkeys:             nonSignerPubkeys,
-		QuorumApks:                   quorumApks,
-		ApkG2:                        core.ConvertToBN254G2Point(blsAggServiceResp.SignersApkG2),
-		Sigma:                        core.ConvertToBN254G1Point(blsAggServiceResp.SignersAggSigG1.G1Point),
-		NonSignerQuorumBitmapIndices: blsAggServiceResp.NonSignerQuorumBitmapIndices,
-		QuorumApkIndices:             blsAggServiceResp.QuorumApkIndices,
-		TotalStakeIndices:            blsAggServiceResp.TotalStakeIndices,
-		NonSignerStakeIndices:        blsAggServiceResp.NonSignerStakeIndices,
-	}
-
-	agg.logger.Info("Threshold reached. Sending aggregated response onchain.",
-		"taskIndex", blsAggServiceResp.TaskIndex,
-	)
-	agg.tasksMu.RLock()
-	task := agg.tasks[blsAggServiceResp.TaskIndex]
-	agg.tasksMu.RUnlock()
-	agg.taskResponsesMu.RLock()
-	taskResponse := agg.taskResponses[blsAggServiceResp.TaskIndex][blsAggServiceResp.TaskResponseDigest]
-	agg.taskResponsesMu.RUnlock()
-	_, err := agg.avsWriter.SendAggregatedResponse(context.Background(), task, taskResponse, nonSignerStakesAndSignature)
+	contractAddress := common.HexToAddress("0xYourContractAddress")
+	tradingContract, err := tradingTaskManager.NewTradingAlgoAVS(contractAddress, nil) // Replace `nil` with actual blockchain client
 	if err != nil {
-		agg.logger.Error("Aggregator failed to respond to task", "err", err)
-	}
-}
-
-// sendNewTask sends a new task to the task manager contract, and updates the Task dict struct
-// with the information of operators opted into quorum 0 at the block of task creation.
-func (agg *Aggregator) sendNewTask(numToSquare *big.Int) error {
-	agg.logger.Info("Aggregator sending new task", "numberToSquare", numToSquare)
-	// Send number to square to the task manager contract
-	newTask, taskIndex, err := agg.avsWriter.SendNewTaskNumberToSquare(context.Background(), numToSquare, types.QUORUM_THRESHOLD_NUMERATOR, types.QUORUM_NUMBERS)
-	if err != nil {
-		agg.logger.Error("Aggregator failed to send number to square", "err", err)
+		agg.logger.Error("Failed to load TradingAlgoAVS contract", "err", err)
 		return err
 	}
 
-	agg.tasksMu.Lock()
-	agg.tasks[taskIndex] = newTask
-	agg.tasksMu.Unlock()
+	// Subscribe to StrategyCreated event
+	strategyCreatedChan := make(chan *tradingTaskManager.TradingAlgoAVSStrategyCreated)
+	strategySub, err := tradingContract.WatchStrategyCreated(nil, strategyCreatedChan, nil, nil)
+	if err != nil {
+		agg.logger.Error("Failed to subscribe to StrategyCreated event", "err", err)
+		return err
+	}
 
-	quorumThresholdPercentages := make(sdktypes.QuorumThresholdPercentages, len(newTask.QuorumNumbers))
-	for i := range newTask.QuorumNumbers {
-		quorumThresholdPercentages[i] = sdktypes.QuorumThresholdPercentage(newTask.QuorumThresholdPercentage)
+	// Subscribe to SubscribedToStrategy event
+	subscribedChan := make(chan *tradingTaskManager.TradingAlgoAVSSubscribedToStrategy)
+	subscriptionSub, err := tradingContract.WatchSubscribedToStrategy(nil, subscribedChan, nil, nil)
+	if err != nil {
+		agg.logger.Error("Failed to subscribe to SubscribedToStrategy event", "err", err)
+		return err
 	}
-	// TODO(samlaf): we use seconds for now, but we should ideally pass a blocknumber to the blsAggregationService
-	// and it should monitor the chain and only expire the task aggregation once the chain has reached that block number.
-	taskTimeToExpiry := taskChallengeWindowBlock * blockTimeSeconds
-	var quorumNums sdktypes.QuorumNums
-	for _, quorumNum := range newTask.QuorumNumbers {
-		quorumNums = append(quorumNums, sdktypes.QuorumNum(quorumNum))
-	}
-	agg.blsAggregationService.InitializeNewTask(taskIndex, newTask.TaskCreatedBlock, quorumNums, quorumThresholdPercentages, taskTimeToExpiry)
+
+	// Process events in a goroutine
+	go func() {
+		for {
+			select {
+			case strategyEvent := <-strategyCreatedChan:
+				agg.processNewStrategy(strategyEvent)
+			case subscriptionEvent := <-subscribedChan:
+				agg.processNewSubscription(subscriptionEvent)
+			case <-ctx.Done():
+				strategySub.Unsubscribe()
+				subscriptionSub.Unsubscribe()
+				return
+			}
+		}
+	}()
+
 	return nil
+}
+
+// Process new strategies when detected
+func (agg *Aggregator) processNewStrategy(event *tradingTaskManager.TradingAlgoAVSStrategyCreated) {
+	agg.logger.Info("New strategy detected", "strategyId", event.StrategyId, "provider", event.Provider)
+
+	agg.strategiesMu.Lock()
+	agg.strategies[event.StrategyId] = tradingTaskManager.Strategy{
+		Id:               event.StrategyId,
+		Provider:         event.Provider,
+		SubscriptionFee:  event.SubscriptionFee,
+		SubscriptionPeriod: event.SubscriptionPeriod,
+		StrategyUid:      event.StrategyUid,
+		Roi:              event.Roi,
+		Profitability:    event.Profitability,
+		Risk:             event.Risk,
+	}
+	agg.strategiesMu.Unlock()
+}
+
+// Process new subscriptions when detected
+func (agg *Aggregator) processNewSubscription(event *tradingTaskManager.TradingAlgoAVSSubscribedToStrategy) {
+	agg.logger.Info("New subscription detected", "strategyId", event.StrategyId, "subscriber", event.Subscriber)
+
+	agg.subscriptionsMu.Lock()
+	if _, exists := agg.subscriptions[event.StrategyId]; !exists {
+		agg.subscriptions[event.StrategyId] = make(map[common.Address]bool)
+	}
+	agg.subscriptions[event.StrategyId][event.Subscriber] = true
+	agg.subscriptionsMu.Unlock()
+}
+
+// Aggregates operator statistics and sends them on-chain if consistent
+func (agg *Aggregator) aggregateResults(strategyId uint256) {
+	agg.strategiesMu.RLock()
+	strategy, exists := agg.strategies[strategyId]
+	agg.strategiesMu.RUnlock()
+
+	if !exists {
+		agg.logger.Error("Strategy not found", "strategyId", strategyId)
+		return
+	}
+
+	// Fetch operator results (Mocked for now)
+	operatorResults := []struct {
+		Roi          *big.Int
+		Profitability *big.Int
+		Risk         *big.Int
+	}{
+		{big.NewInt(5), big.NewInt(80), big.NewInt(10)},
+		{big.NewInt(5), big.NewInt(79), big.NewInt(11)},
+		{big.NewInt(5), big.NewInt(81), big.NewInt(9)},
+	}
+
+	// Calculate mean and standard deviation
+	// (Add standard deviation check logic here)
+
+	// If within standard deviation, submit to blockchain
+	agg.logger.Info("Sending aggregated trading statistics on-chain.", "strategyId", strategyId)
+	_, err := agg.avsWriter.SendAggregatedTradingResults(context.Background(), strategy)
+	if err != nil {
+		agg.logger.Error("Failed to send aggregated results", "err", err)
+	}
 }
