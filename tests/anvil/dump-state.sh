5cfa2363cd42c91aff9c9a5dbb538931718f70dc
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-03-13 14:27:21 -0300

    fix: export PRIVATE_KEY in deploy scripts

--- tests/anvil/dump-state.sh
+++ tests/anvil/dump-state.sh
@@ -13,7 +13,7 @@ set -a
 source $parent_path/utils.sh
 # we overwrite some variables here because should always deploy to anvil (localhost)
 RPC_URL=http://localhost:8545
-PRIVATE_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+export PRIVATE_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
 set +a
 
 
