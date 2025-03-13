037274c00e27721837c00a03f00c5f4c8d053142
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-03-13 16:58:58 -0300

    fix: remove unused solidity scripts

--- tests/anvil/deploy-avs.sh
+++ tests/anvil/deploy-avs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 RPC_URL=http://localhost:8545
 export PRIVATE_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
 
@@ -15,7 +17,3 @@ root_dir=$(realpath "$parent_path/../..")
 # Deploy Contracts
 cd "$root_dir/contracts"
 forge script script/IncredibleSquaringDeployer.s.sol --rpc-url $RPC_URL --private-key $PRIVATE_KEY --broadcast --slow
-
-forge script script/SetupPayments.s.sol --rpc-url $RPC_URL --broadcast --private-key $PRIVATE_KEY
-
-forge script script/OperatorDirectedPayments.s.sol --rpc-url $RPC_URL --broadcast --slow --private-key $PRIVATE_KEY
