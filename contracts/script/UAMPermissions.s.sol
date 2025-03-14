a822abdbad274d6b9d6948c7fa0e15af5ed76813
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-03-14 16:15:04 -0300

    feat: bump middleware to 1.1.1 version (#145)
    * chore: bump middleware version to 1.1.1
    
    * feat: update incredibleDeploymentLib with new middleware methods
    
    * feat: add update AVS metadata in permissions
    
    * chore: json states
    
    * chore: generate bindings
    
    * feat: run forge fmt
    
    * feat: add payments.json
    
    * chore: run forge fmt

--- contracts/script/UAMPermissions.s.sol
+++ contracts/script/UAMPermissions.s.sol
@@ -59,6 +59,15 @@ contract UAMPermissions is Script {
             AllocationManager.slashOperator.selector
         );
 
+        // This should be in another contract
+        serviceManager.setAppointee(
+            deployer, coreData.allocationManager, AllocationManager.updateAVSMetadataURI.selector
+        );
+
+        _allocationManager.updateAVSMetadataURI(
+            deploymentData.incredibleSquaringServiceManager, "metadataURI"
+        );
+
         vm.stopBroadcast();
     }
 }
