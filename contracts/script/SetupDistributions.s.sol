58ac90ea70e88c1e42cd2b724198344ab3c94e00
maximopalopoli <mpalopoli@fi.uba.ar>
2025-03-17 10:04:22 -0300

    Avoid time between claims generation

--- contracts/script/SetupDistributions.s.sol
+++ contracts/script/SetupDistributions.s.sol
@@ -36,7 +36,10 @@ contract SetupDistributions is Script, Test {
 
     uint32 constant CALCULATION_INTERVAL_SECONDS = 1 days;
     uint256 constant NUM_TOKEN_EARNINGS = 1;
-    uint32 constant DURATION = 1;
+    //duration MUST be a multiple of CALCULATION_INTERVAL_SECONDS .
+    //https://github.com/Layr-Labs/eigenlayer-contracts/blob/865e723a6b5c634cf45cce1817dec0ea95f0e03b/src/contracts/core/RewardsCoordinator.sol#L439
+    uint32 constant DURATION = 172_800;
+    uint32 constant REWARDS_END_TIMESTAMP_GAP = 1 days;
     uint256 constant NUM_EARNERS = 8;
 
     uint32 numPayments = 8;
@@ -96,7 +99,9 @@ contract SetupDistributions is Script, Test {
         uint256 nonce = rewardsCoordinator.getDistributionRootsLength();
         amountPerPayment = uint32(amountPerPayment * (nonce + 1));
 
-        createOperatorDirectedAVSRewardsSubmissions(numPayments, amountPerPayment, startTimestamp);
+        createOperatorDirectedAVSRewardsSubmissions(
+            numPayments, amountPerPayment, startTimestamp, DURATION
+        );
         vm.stopBroadcast();
         vm.startBroadcast(deployer);
         earners = _getEarners(deployer);
@@ -105,18 +110,19 @@ contract SetupDistributions is Script, Test {
     }
 
     function _checkStartTimestamp() internal {
-        if (rewardsCoordinator.currRewardsCalculationEndTimestamp() == 0) {
-            startTimestamp =
-                uint32(block.timestamp) - (uint32(block.timestamp) % CALCULATION_INTERVAL_SECONDS);
-        } else {
-            startTimestamp = rewardsCoordinator.currRewardsCalculationEndTimestamp() - DURATION
-                + CALCULATION_INTERVAL_SECONDS;
-        }
+        // Go back 4 days
+        uint256 targetStartTimestamp = block.timestamp - 4 days;
+        // Start Timestamp must be a multiple of CALCULATION_INTERVAL_SECONDS
+        uint32 diff = (uint32(targetStartTimestamp) % CALCULATION_INTERVAL_SECONDS);
+        startTimestamp = uint32(targetStartTimestamp) - diff;
 
-        endTimestamp = startTimestamp + 1;
-
+        endTimestamp = uint32(block.timestamp) - REWARDS_END_TIMESTAMP_GAP;
+        emit log_named_uint("startTimestamp", startTimestamp);
+        emit log_named_uint("endTimestamp", endTimestamp);
+        emit log_named_uint("block.timestamp", block.timestamp);
+        emit log_named_uint("MAX_RETROACTIVE_LENGTH", rewardsCoordinator.MAX_RETROACTIVE_LENGTH());
         if (endTimestamp > block.timestamp) {
-            revert("End timestamp is in the future.  Please wait to generate new payments.");
+            revert("RewardsEndTimestampNotElapsed.  Please wait to generate new payments.");
         }
     }
 
@@ -159,14 +165,14 @@ contract SetupDistributions is Script, Test {
     function createOperatorDirectedAVSRewardsSubmissions(
         uint256 numPayments,
         uint256 amountPerPayment,
-        uint32 startTimestamp
+        uint32 startTimestamp,
+        uint32 duration
     ) public {
         MockERC20(incredibleSquaringDeployment.token).mint(deployer, amountPerPayment * numPayments);
         MockERC20(incredibleSquaringDeployment.token).increaseAllowance(
             incredibleSquaringDeployment.incredibleSquaringServiceManager,
             amountPerPayment * numPayments
         );
-        uint32 duration = 0;
         address[] memory operators = new address[](2);
         operators[0] = operator1;
         operators[1] = operator2;
