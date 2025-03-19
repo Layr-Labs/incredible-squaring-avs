2b601875c5400b6c8be671b3c7763e4ff5e6767d
maximopalopoli <mpalopoli@fi.uba.ar>
2025-03-19 17:57:31 -0300

    Add operaor directed claim to script

--- scripts/rewards-script-check.sh
+++ scripts/rewards-script-check.sh
@@ -16,19 +16,36 @@ if [ "$initialBalance" -ne 0 ]; then
     exit 2
 fi
 
-# Create and claim distribution root
+# Create and claim normal distribution root
 echo "Creating distribution root:"
 make create-avs-distributions-root
 
 echo "Claiming distribution root:"
 make claim-distributions
 
-# Check that after claims, claimer balance in token is 100
+# Check that after claim, claimer balance in token is 100
 balanceAfterClaim=$(make claimer-account-token-balance | tail -n1 | tr -d '[:space:]')
 
-echo "Balance after claim: '$balanceAfterClaim'"
+echo "Balance after first claim: '$balanceAfterClaim'"
 
 if [ "$balanceAfterClaim" -ne 100 ]; then
     echo "After first claim, claimer balance in token should be 100"
+    exit 3
+fi
+
+# Create and claim operator directed distribution root
+echo "Creating operator directed distribution root:"
+make create-operator-directed-distributions-root
+
+echo "Claiming distribution root:"
+make claim-distributions
+
+# Check that after another claim, claimer balance in token is 200
+balanceAfterClaim=$(make claimer-account-token-balance | tail -n1 | tr -d '[:space:]')
+
+echo "Balance after second claim: '$balanceAfterClaim'"
+
+if [ "$balanceAfterClaim" -ne 200 ]; then
+    echo "After first claim, claimer balance in token should be 200"
     exit 3
 fi
