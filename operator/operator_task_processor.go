c56741e09e878546780d082734040df9d2027186
maximopalopoli <mpalopoli@fi.uba.ar>
2025-04-11 09:22:28 -0300

    Remove temporarily the times failing logic

--- operator/operator_task_processor.go
+++ operator/operator_task_processor.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"math/big"
-	"time"
 
 	sdkaggregator "github.com/Layr-Labs/eigensdk-go/aggregator"
 	"github.com/Layr-Labs/eigensdk-go/logging"
@@ -12,20 +11,14 @@ import (
 	"github.com/Layr-Labs/incredible-squaring-avs/aggregator"
 	cstaskmanager "github.com/Layr-Labs/incredible-squaring-avs/contracts/bindings/IncredibleSquaringTaskManager"
 	"github.com/ethereum/go-ethereum/core/types"
-	"golang.org/x/exp/rand"
 )
 
-type OperatorTaskProcessor struct{
-	logger        logging.Logger
-
-	
-	// If bigger than zero, submits wrong responses that many times every 100
-	timesFailing int
+type OperatorTaskProcessor struct {
+	logger logging.Logger
 }
 
 func NewOperatorTaskProcessor(c sdkoperator.OperatorConfig, logger logging.Logger) OperatorTaskProcessor {
 	return OperatorTaskProcessor{
-		timesFailing: c.TimesFailing,
 		logger: logger,
 	}
 }
@@ -52,7 +45,7 @@ func (otp OperatorTaskProcessor) ProcessNewTaskCreatedLog(
 	if err != nil {
 		return nil, fmt.Errorf("error unpacking the log: %w", err)
 	}
-	
+
 	otp.logger.Debug("Received new task", "task", newTaskCreatedLog)
 	otp.logger.Info("Received new task",
 		"numberToBeSquared", newTaskCreatedLog.Task.NumberToBeSquared,
@@ -64,14 +57,14 @@ func (otp OperatorTaskProcessor) ProcessNewTaskCreatedLog(
 
 	numberSquared := big.NewInt(0).Exp(newTaskCreatedLog.Task.NumberToBeSquared, big.NewInt(2), nil)
 
-	if otp.timesFailing > 0 {
-		rand.Seed(uint64((time.Now().UnixNano())))
-		num := rand.Intn(100)
-		if num < otp.timesFailing {
-			numberSquared = big.NewInt(908243203843)
-			otp.logger.Info("Operator computed wrong task result")
-		}
-	}
+	// if otp.timesFailing > 0 {
+	// 	rand.Seed(uint64((time.Now().UnixNano())))
+	// 	num := rand.Intn(100)
+	// 	if num < otp.timesFailing {
+	// 		numberSquared = big.NewInt(908243203843)
+	// 		otp.logger.Info("Operator computed wrong task result")
+	// 	}
+	// }
 
 	taskResponse := aggregator.IncredibleSquaringTaskResponse{
 		ReferenceTaskIndex: newTaskCreatedLog.TaskIndex,
