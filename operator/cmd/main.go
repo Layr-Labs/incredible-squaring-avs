4dfc37f6bd512c124ffe6d22b71be2db32526311
maximopalopoli <mpalopoli@fi.uba.ar>
2025-04-11 09:27:18 -0300

    Use SDK operator task processor interface on operator main

--- operator/cmd/main.go
+++ operator/cmd/main.go
@@ -8,7 +8,11 @@ import (
 
 	"github.com/urfave/cli"
 
+	sdklogging "github.com/Layr-Labs/eigensdk-go/logging"
+	sdkoperator "github.com/Layr-Labs/eigensdk-go/operator"
+	"github.com/Layr-Labs/incredible-squaring-avs/aggregator"
 	commonincredible "github.com/Layr-Labs/incredible-squaring-avs/common"
+	cstaskmanager "github.com/Layr-Labs/incredible-squaring-avs/contracts/bindings/IncredibleSquaringTaskManager"
 	"github.com/Layr-Labs/incredible-squaring-avs/core/config"
 	"github.com/Layr-Labs/incredible-squaring-avs/operator"
 	"github.com/Layr-Labs/incredible-squaring-avs/types"
@@ -44,14 +48,39 @@ func operatorMain(ctx *cli.Context) error {
 	log.Println("Config:", string(configJson))
 
 	log.Println("initializing operator")
-	operator, err := operator.NewOperatorFromConfig(nodeConfig)
+
+	logger, err := sdklogging.NewZapLogger(sdklogging.Production) // Change here if want to change logging level
+	if err != nil {
+		return err
+	}
+
+	taskManagerAbi, err := cstaskmanager.ContractIncredibleSquaringTaskManagerMetaData.GetAbi()
+	if err != nil {
+		logger.Fatalf(err.Error())
+	}
+
+	blockHash := taskManagerAbi.Events["NewTaskCreated"].ID
+
+	operatorConfig := sdkoperator.OperatorConfig{
+		OperatorAddress:               nodeConfig.OperatorAddress,
+		OperatorStateRetrieverAddress: nodeConfig.OperatorStateRetrieverAddress,
+		ServiceManagerAddress:         nodeConfig.IncredibleSquaringServiceManager,
+		AVSRegistryCoordinatorAddress: nodeConfig.AVSRegistryCoordinatorAddress,
+		EthRpcUrl:                     nodeConfig.EthRpcUrl,
+		EthWsUrl:                      nodeConfig.EthWsUrl,
+		BlsPrivateKeyStorePath:        nodeConfig.BlsPrivateKeyStorePath,
+		AggregatorServerIpPortAddress: nodeConfig.AggregatorServerIpPortAddress,
+		RegisterOnStartup: true,
+	}
+	operatorTaskProcessor := operator.NewOperatorTaskProcessor(operatorConfig, logger)
+	operator, err := sdkoperator.NewOperatorFromConfig(operatorConfig, blockHash, operatorTaskProcessor, logger)
 	if err != nil {
 		return err
 	}
 	log.Println("initialized operator")
 
 	log.Println("starting operator")
-	err = operator.Start(context.Background())
+	err = operator.Start(context.Background(), &aggregator.IncredibleSquaringTaskResponse{})
 	if err != nil {
 		return err
 	}
