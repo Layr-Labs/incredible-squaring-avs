9fa66cbe45c621e669ec48f1a69c1ff5ac38bd9b
maximopalopoli <mpalopoli@fi.uba.ar>
2025-04-22 09:46:00 -0300

    Adapt to the change of challenger logic to verifier and output use

--- tests/integration/integration_test.go
+++ tests/integration/integration_test.go
@@ -166,14 +166,14 @@ func TestIntegration(t *testing.T) {
 		Logger:   config.Logger,
 	}
 
-	challengerLogicImpl, err := challenger.NewChallengerLogicImpl(config)
+	challengerVerifier, err := challenger.NewChallengerVerifierImpl(config)
 	if err != nil {
 		config.Logger.Fatalf("Failed to create challenger logic from config: %v", err)
 	}
 
 	challenger, err := sdkchallenger.NewChallenger(
 		challenferCfg,
-		challengerLogicImpl,
+		challengerVerifier,
 		newTaskEventHash,
 		taskRespondedEventHash,
 		taskManagerAbi,
