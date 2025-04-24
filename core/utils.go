ded77cd2dcb085fc285b61b84567664cd50097bd
maximopalopoli <mpalopoli@fi.uba.ar>
2025-04-24 11:56:15 -0300

    Remove unused utils

--- core/utils.go
+++ core/utils.go
@@ -5,57 +5,7 @@ import (
 
 	"github.com/Layr-Labs/eigensdk-go/crypto/bls"
 	cstaskmanager "github.com/Layr-Labs/incredible-squaring-avs/contracts/bindings/IncredibleSquaringTaskManager"
-	"github.com/ethereum/go-ethereum/accounts/abi"
-	"golang.org/x/crypto/sha3"
 )
-
-// this hardcodes abi.encode() for cstaskmanager.IIncredibleSquaringTaskManagerTaskResponse
-// unclear why abigen doesn't provide this out of the box...
-func AbiEncodeTaskResponse(h *cstaskmanager.IIncredibleSquaringTaskManagerTaskResponse) ([]byte, error) {
-
-	// The order here has to match the field ordering of cstaskmanager.IIncredibleSquaringTaskManagerTaskResponse
-	taskResponseType, err := abi.NewType("tuple", "", []abi.ArgumentMarshaling{
-		{
-			Name: "referenceTaskIndex",
-			Type: "uint32",
-		},
-		{
-			Name: "numberSquared",
-			Type: "uint256",
-		},
-	})
-	if err != nil {
-		return nil, err
-	}
-	arguments := abi.Arguments{
-		{
-			Type: taskResponseType,
-		},
-	}
-
-	bytes, err := arguments.Pack(h)
-	if err != nil {
-		return nil, err
-	}
-
-	return bytes, nil
-}
-
-// GetTaskResponseDigest returns the hash of the TaskResponse, which is what operators sign over
-func GetTaskResponseDigest(h *cstaskmanager.IIncredibleSquaringTaskManagerTaskResponse) ([32]byte, error) {
-
-	encodeTaskResponseByte, err := AbiEncodeTaskResponse(h)
-	if err != nil {
-		return [32]byte{}, err
-	}
-
-	var taskResponseDigest [32]byte
-	hasher := sha3.NewLegacyKeccak256()
-	hasher.Write(encodeTaskResponseByte)
-	copy(taskResponseDigest[:], hasher.Sum(nil)[:32])
-
-	return taskResponseDigest, nil
-}
 
 // BINDING UTILS - conversion from contract structs to golang structs
 
